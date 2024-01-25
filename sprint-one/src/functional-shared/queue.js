bde862e9d6317b60326a53214d96665f61e7adf1
Eric Yap <ericy2296@gmail.com>
2024-01-25 10:24:39 -0800

    solo work stacks/queues

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.front = 0;
+  queue.back = 0;
+
+  // Extend the queue object
+  _.extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  },
+  dequeue: function() {
+    if (this.front === this.back) {
+      return undefined;
+    }
+    var result = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return result;
+  },
+  size: function() {
+    return this.back - this.front;
+  }
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,27 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var back = 0; //position of last element in queue;
+  var front = 0; //position of first element in queue
+
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    back++;
   };
 
   someInstance.dequeue = function() {
+    if (back === front) {
+      return undefined;
+    }
+    var result = storage[front];
+    delete storage[front];
+    front++;
+    return result;
+
   };
 
   someInstance.size = function() {
+    return back - front;
   };
 
   return someInstance;
